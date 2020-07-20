04e4952dfb29e1d03a9b50801a5cd2a6a32fe8c5
Freezefaz <m.fareez.aziz@gmail.com>
2020-07-20 04:57:09 +0000

    Commit 6
    Index
    - Removed bootstrap stylesheet as it was interfering with map generation
    - Added leaflet market cluster script
    Script
    - Created random latlng function
    - Added random marker when #start-btn clicked

--- script.js
+++ script.js
@@ -16,6 +16,36 @@ $(".nav-button").click(function(){
 
 // Start of Game Page
 
+$(function() {
+// Create random position
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+// for Map loading ****
+let singapore = [ 1.376950, 103.806926];
+// no zoom control options
+let map = L.map("map").setView(singapore, 12);
+// Setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 12,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
 // For adding pokemon picture and name into status bar
 let url = "https://pokeapi.co/api/v2/pokemon/";
 let wantedPokemonNumber = Math.floor((Math.random()*152)+1);
@@ -28,20 +58,12 @@ start.addEventListener('click', function(){
         <img src="${response.data.sprites.front_default}"/>`
         let identityDiv = document.querySelector("#pokemon-profile");
         identityDiv.innerHTML = HTMLfragment;
+        let wantedPokemonMarkerPositon = getRandomLatLng(map);
+        let wantedPokemonMarker=L.marker(wantedPokemonMarkerPositon);
+        wantedPokemonMarker.bindPopup(`<p>${response.data.name}</p>`)
+        // wantedPokemonMarker = L.marker([getRandomLatLng(map)]);
+        wantedPokemonMarker.addTo(map);
+        })
     })
 })
-
-// for Map loading ****
-let singapore = [ 1.29,103.85]
-// no zoom control options
-let map = L.map("map", {zoomControl: false}).setView(singapore, 12);
-// Setup the tile layers
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 12,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-}).addTo(map);
-
+ 
