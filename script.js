d1386ae8a287807a2e4d1adccfaab97ea19d508f
Freezefaz <m.fareez.aziz@gmail.com>
2020-07-20 02:37:00 +0000

    Commit 3
    - Added h1 for index
    - Created function to allow pages to swtich with one another
    - Added styling to the pages
    For Home
    - Created image folder and added logo
    - Created #logo, #title and #rules

---
+++ script.js
@@ -0,0 +1,14 @@
+// For buttons to control switching of pages
+function hidePages() {
+    let pages = $(".page");
+    for (let p of pages) {
+        $(p).removeClass("shown");
+        $(p).addClass("hidden")
+    }
+}
+$(".nav-button").click(function(){
+    let pageNumber = $(this).data("page");
+    hidePages();
+    $(`#page-${pageNumber}`).addClass("shown");
+    $(`#page-${pageNumber}`).removeClass("hidden");
+})
